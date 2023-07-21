5c63b2d6cb0dcc723de540b60de2fcc7e7ef72a0
Mário Roberto <python.mario@gmail.com>
2023-07-21 20:57:31 -0300

    Jogo da cobrinha vs1

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = 'red';
+// ctx.fillRect(300, 100, 50, 100);
+
+/*tamanho do quadradinhos*/
+const size = 30;
+
+const snake = [{ x: 0, y: 0}]
+
+let direction, loopId;
+
+const drawSnake = () => {
+  ctx.fillStyle = "#ddd";
+  // ctx.fillRect(snake[0].x, snake[0].y, size, size);
+  snake.forEach((position, index) => {
+    if (index == snake.length - 1) {
+      ctx.fillStyle = "white";
+    }
+
+    ctx.fillRect(position.x, position.y, size, size);
+  });
+};
+
+const moveSnake = () => {
+  if (!direction) return;
+
+  const head = snake[snake.length - 1];
+
+  if (direction == "right") {
+    snake.push({ x: head.x + size, y: head.y });
+  }
+
+  if (direction == "left") {
+    snake.push({ x: head.x - size, y: head.y });
+  }
+
+  if(direction == 'down'){
+    snake.push({x: head.x, y: head.y + size });
+ }
+
+ if(direction == 'up'){
+    snake.push({x: head.x, y: head.y - size });
+ }
+
+  snake.shift();
+};
+
+const gameLoop = () => {
+    clearInterval(loopId);
+
+    ctx.clearRect(0, 0, 600, 600);
+
+    moveSnake();
+    drawSnake();
+loopId = setTimeout(() => {
+        gameLoop();
+    }, 300)
+}
+
+gameLoop();
+
+document.addEventListener("keydown", ({ key }) => {
+    // console.log(event.key);
+
+    if (key == "ArrowRight" && direction != "left") {
+        direction = "right" 
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = "left" 
+    } 
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = "down"  
+    }
+
+    if (key == "ArrowUp" && direction != "down") {
+        direction = "up" 
+    }
+    
+})
+
+
